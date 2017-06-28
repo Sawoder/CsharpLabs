0875df2e145ef8ca32d6855289910d47d1a98af6
Sawoder <sawoder@gmail.com>
2017-06-28 16:34:43 +0300

    Delete test files

--- Library/UnitTest/UnitTest.bat
+++
@@ -1,4 +0,0 @@
-chcp 861
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\MSTest.exe" /testcontainer:"C:\Users\delfi\Documents\Visual Studio 2017\Projects\ConsoleApp1\UnitTestProject1\bin\Debug\UnitTestProject1.dll" > tmp.txt
-cmd /U /C type tmp.txt > UnitTestLog.txt
-del tmp.txt
