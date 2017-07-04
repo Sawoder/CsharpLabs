ea159469d883533313519b00c72529635eae726d
Sawoder <sawoder@gmail.com>
2017-07-05 01:36:39 +0300

    Bi-Directional LinkedList implementation with UnitTest

---
+++ LinkedList/MyLinkedList.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyLinkedList
+{
+    public class MyLinkedList<T> : ICollection<T>
+    {
+        public event Action<T> Added;
+        public event Action<T> Removed;
+        public event Action<string> Cleared;
+        
+        public int Count { get; private set; }
+        public bool IsReadOnly { get { return false; } }
+
+        public Node First { get; private set; }
+        public Node Last { get; private set; }
+
+        public class Node
+        {
+            public T Item;
+            public Node Next { get; set; }
+            public Node Prev { get; set; }
+
+            public Node(T item)
+            {
+                Item = item;
+            }
+        }
+        
+        public void Add(T item)
+        {
+            Node node = new Node(item);
+            if (Count == 0) First = node;
+            else
+            {
+                Last.Next = node;
+                node.Prev = Last;
+            }
+            Last = node;
+            Count++;
+            Added?.Invoke(item);
+        }
+
+        public void Clear()
+        {
+            First = null;
+            Last = null;
+            Count = 0;
+            Cleared?.Invoke("Clear");
+        }
+
+        public bool Contains(T item)
+        {
+            Node current = First;
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Node current = First;
+            while (current != null)
+            {
+                array[arrayIndex++] = current.Item;
+                current = current.Next;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            Node prev = null;
+            Node current = First;
+
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    if (prev != null)
+                    {
+                        prev.Next = current.Next;
+                        if (current.Next == null) Last = prev;
+                        else current.Next.Prev = prev;
+                        Count--;
+                    }
+                    else
+                    {
+                        if (Count != 0)
+                        {
+                            First = First.Next;
+                            Count--;
+                            if (Count == 0) Last = null;
+                            else First.Prev = null;
+                        }
+                    }
+                    Removed?.Invoke(item);
+                    return true;
+                }
+                prev = current;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = First;
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public bool Insert(T item, T index)
+        {
+            Node current = First;
+            Node node = new Node(item);
+
+            while (current != null)
+            {
+                if (current.Item.Equals(index))
+                {
+                    node.Next = current;
+                    node.Prev = current.Prev;
+                    if (current.Prev != null)
+                    {
+                        current.Prev.Next = node;
+                    }
+                    current.Prev = node;
+
+                    if (node.Prev == null)
+                    {
+                        First = node;
+                    }
+                    Count++;
+                    Added?.Invoke(item);
+                    return true;
+                }
+                current = current.Next;
+            }
+            
+            return false;
+        }
+    }
+}
---
+++ LinkedList/UnitTest/UnitTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLinkedList;
+using System.Collections.Generic;
+
+namespace UnitTestMyLinkedList
+{
+    [TestClass]
+    public class UnitTest
+    {
+        MyLinkedList<int> testList;
+
+        [TestInitialize]
+        public void Test_Init()
+        {
+            testList = new MyLinkedList<int>();
+        }
+
+        [TestMethod]
+        public void List_Add()
+        {
+            testList.Add(0);
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            Assert.AreEqual(0, testList.First.Item);
+            Assert.AreEqual(3, testList.Last.Item);
+            Assert.AreEqual(4, testList.Count);
+        }
+
+        [TestMethod]
+        public void List_Contains()
+        {
+            testList.Add(0);
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            Assert.IsTrue(testList.Contains(2));
+            Assert.IsFalse(testList.Contains(4));
+
+        }
+
+        [TestMethod]
+        public void List_CopyTo()
+        {
+            int[] array = new int[10];
+            testList.Add(1);
+            testList.Add(1);
+            testList.Add(1);
+            testList.Add(1);
+            testList.CopyTo(array, 4);
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i < 4)
+                    Assert.IsTrue(array[i] == 0);
+                else if (i >= 4 && i < array.Length - 2)
+                    Assert.IsTrue(array[i] == 1);
+            }
+
+        }
+        [TestMethod]
+        public void List_Remove()
+        {
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            testList.Add(4);
+            testList.Remove(2);
+            Assert.AreEqual(3, testList.Count);
+            Assert.IsTrue(testList.First.Next.Item == 3);
+        }
+        [TestMethod]
+        public void List_GetEnumerator()
+        {
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            testList.Add(4);
+            int i = 1;
+            foreach (int a in testList)
+            {
+                Assert.AreEqual(a, i++);
+            }
+        }
+        [TestMethod]
+        public void List_Insert()
+        {
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            testList.Add(4);
+            testList.Insert(7, 3);
+            Assert.AreEqual(5, testList.Count);
+            Assert.AreEqual(7, testList.First.Next.Next.Item);
+        }
+
+        [TestMethod]
+        public void List_Actions()
+        {
+            List<string> events = new List<string>();
+            testList.Added += delegate (int add) { events.Add($"Add new Item = {add}"); };
+            testList.Removed += delegate (int rem) { events.Add($"Remove Item = {rem}"); };
+            testList.Cleared += delegate (string str) { events.Add(str); };
+            testList.Add(1);
+            testList.Add(2);
+            testList.Remove(2);
+            testList.Clear();
+            Assert.AreEqual("Add new Item = 1", events[0]);
+            Assert.AreEqual("Remove Item = 2", events[2]);
+            Assert.AreEqual("Clear", events[3]);
+        }
+
+        [TestCleanup]
+        public void Test_Clean()
+        {
+            testList = null;
+        }
+
+    }
+}
